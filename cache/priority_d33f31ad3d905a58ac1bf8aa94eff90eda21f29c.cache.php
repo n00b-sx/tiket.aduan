8a0c40c253538935566e4db7857c0dbf541a0901
n00b.sx <syahrul.noor@gmail.com>
2025-12-01 09:45:20 +0800

    Baru Up

---
+++ cache/priority_d33f31ad3d905a58ac1bf8aa94eff90eda21f29c.cache.php
@@ -0,0 +1,34 @@
+<?php if (!defined('IN_SCRIPT')) {die();} $hesk_settings['priorities']=array (
+  3 => 
+  array (
+    'id' => '3',
+    'name' => 'Rendah',
+    'color' => '#71a5ec',
+    'can_customers_select' => '1',
+    'priority_order' => '1',
+  ),
+  2 => 
+  array (
+    'id' => '2',
+    'name' => 'Menengah',
+    'color' => '#3abb7a',
+    'can_customers_select' => '1',
+    'priority_order' => '2',
+  ),
+  1 => 
+  array (
+    'id' => '1',
+    'name' => 'Tinggi',
+    'color' => '#fac500',
+    'can_customers_select' => '1',
+    'priority_order' => '3',
+  ),
+  0 => 
+  array (
+    'id' => '0',
+    'name' => 'Kritis',
+    'color' => '#e74441',
+    'can_customers_select' => '0',
+    'priority_order' => '4',
+  ),
+);
